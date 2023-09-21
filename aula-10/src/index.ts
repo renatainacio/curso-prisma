fd887d136432004ec97d711637567716dc87ce90
Renata Inacio <revazgauska@gmail.com>
2023-09-21 11:35:31 -0300

    exercicio aula 10

---
+++ aula-10/src/index.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response } from "express";
+import prisma from "./database";
+import { Prisma } from "@prisma/client";
+
+const app = express();
+
+type PetResult = {
+  owner: string;
+  pet: string;
+  animal: string;
+}
+
+app.get("/pets/owner/:ownerName", async (req: Request, res: Response) => {
+  const { ownerName } = req.params;
+  try {
+    const result = await prisma.$queryRaw<PetResult>(
+      Prisma.sql`SELECT * FROM pet JOIN people on pet."personId" = people.id WHERE people.name = ${ownerName}`
+    ); // TODO: Implemente a query
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+})
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is up and running or port ${port}`);
+})
